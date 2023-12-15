3a57fbc2880a33c4338d41a22352f7d8a5e25652
Vlad Teteruk <vladteteruk01@gmail.com>
2023-12-15 21:38:12 +0200

    Add RenovationLead dataclass & implement page scrolling

--- parsers.py
+++ parsers.py
@@ -1,4 +1,17 @@
+import dataclasses
+import time
+
 from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
+@dataclasses.dataclass
+class RenovationLead:
+    company_name: str
+    company_number: str | None
+    company_website: str = None
+    business_owners: list[str] = None
 
 
 class ChromeParser:
@@ -9,7 +22,22 @@ class ChromeParser:
 
 
 class GoogleMapsParser(ChromeParser):
-    pass
+    def scroll_to_the_end_of_sidebar(self) -> None:
+        side_panel = self.driver.find_element(By.XPATH, "//div[@role='feed']")
+
+        while True:
+            self.driver.execute_script("arguments[0].scrollTop += 500;", side_panel)
+            time.sleep(0.1)
+            try:
+                self.driver.find_element(By.CLASS_NAME, "HlvSq")
+                break
+            except NoSuchElementException:
+                pass
+
+    def main(self, google_maps_url: str) -> list[RenovationLead]:
+        self.driver.get(google_maps_url)
+
+        self.scroll_to_the_end_of_sidebar()
 
 
 class OwnersParser(ChromeParser):
