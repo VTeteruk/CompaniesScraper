24f4bc15d33373b716db573c0574932ec45e10fd
Vlad Teteruk <vladteteruk01@gmail.com>
2023-12-19 19:17:41 +0200

    Fix: use function in starter.py & Add CompaniesDataParser class to optimize code

---
+++ parsers/companies_data_parser.py
@@ -0,0 +1,6 @@
+from parsers.google_maps_parser import GoogleMapsParser
+from parsers.owners_parser import OwnersParser
+
+
+class CompaniesDataParser(GoogleMapsParser, OwnersParser):
+    pass
--- starter.py
+++ starter.py
@@ -1,16 +1,19 @@
-from parsers.google_maps_parser import GoogleMapsParser
-from parsers.owners_parser import OwnersParser
+from parsers.companies_data_parser import CompaniesDataParser
 from utilities.saver import save_data_to_excel, save_data_to_csv
 
+
+def main(site_url: str) -> None:
+    parser = CompaniesDataParser()
+    data = parser.main(site_url)  # TODO: use more accurate class method
+    parser.find_owners(data)
+    parser.destroy()
+
+    save_data_to_excel(data, file_name="renovation_leads")
+    save_data_to_csv(data, file_name="renovation_leads")
+
+
 if __name__ == "__main__":
-    google_maps_parser = GoogleMapsParser()
-    data = google_maps_parser.main(
-        "https://www.google.com/maps/search/"
+    main(
+        site_url="https://www.google.com/maps/search/"
         "renovation/@52.583251,-0.2907134,12.05z?entry=ttu"
     )
-    owners_parser = OwnersParser()
-    owners_parser.find_owners(data)
-
-    save_data_to_excel(data, "renovation_leads")
-    save_data_to_csv(data, "renovation_leads")
-    google_maps_parser.destroy()
