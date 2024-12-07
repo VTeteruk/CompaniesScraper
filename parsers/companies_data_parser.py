e90eb03a2ce1f7c1fd8f83d258d18130d70cd2cf
Vlad Teteruk <vladteteruk01@gmail.com>
2024-12-07 14:26:09 +0100

    Fix: make it pretty

--- parsers/companies_data_parser.py
+++
@@ -1,6 +0,0 @@
-from parsers.async_owners_parser import AsyncOwnersParser
-from parsers.google_maps_parser import GoogleMapsParser
-
-
-class CompaniesDataParser(GoogleMapsParser, AsyncOwnersParser):
-    pass
